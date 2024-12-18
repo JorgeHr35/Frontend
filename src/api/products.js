44d2b1c91d41c26c897f56a50d5969ce508e3a91
JorgeHr35 <147641123+JorgeHr35@users.noreply.github.com>
2024-12-18 00:27:52 -0600

    Update products.js

--- src/api/products.js
+++ src/api/products.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:4000/api/products";
+import API from "./axiosInstance";
 
 // Obtener productos
-export const getAllProducts = async () => (await axios.get(API_URL)).data;
+export const getAllProducts = async () => (await API.get("/products")).data;
 
 // Obtener producto por ID
-export const getProductById = async (id) => (await axios.get(`${API_URL}/${id}`)).data;
+export const getProductById = async (id) => (await API.get(`/products/${id}`)).data;
 
 // Crear producto
 export const createProduct = async (productData) => {
-  const response = await axios.post(API_URL, productData, {
+  const response = await API.post("/products", productData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return response.data;
 };
 
+// Actualizar producto
 export const updateProduct = async (id, productData) => {
-  const response = await axios.put(`${API_URL}/${id}`, productData, {
+  const response = await API.put(`/products/${id}`, productData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return response.data;
 };
 
 // Eliminar producto
-export const deleteProduct = async (id) => (await axios.delete(`${API_URL}/${id}`)).data;
+export const deleteProduct = async (id) => (await API.delete(`/products/${id}`)).data;
+
